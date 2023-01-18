bf98ca49b8f640908c3281ddca0ed2a42cf75ce2
nninzy <116625040+nninzy@users.noreply.github.com>
2023-01-18 15:39:29 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 116 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,5 +1,6 @@
-t = int(input())
+import sys
+t = int(sys.stdin.readline())
 def rev(item) :
     return item[::-1]
 for _ in range(t) :
-    print(*list(map(rev, input().split())))
+    print(*list(map(rev, sys.stdin.readline().rstrip().split())))
