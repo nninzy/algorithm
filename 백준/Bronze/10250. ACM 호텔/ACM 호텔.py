16052ea7bb544bf00fda6004e11701b8a618b199
MinJee Kim <116625040+nninzy@users.noreply.github.com>
2023-01-28 21:42:43 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,12 +1,5 @@
 T = int(input())
 for _ in range(T) :
     h, w, n = map(int, input().split())
-    if n % h == 0 :
-        my_w, my_h = n // h, h
-    else :
-        my_w, my_h = (n // h + 1), n % h
-    if w < 100 :
-        answer = my_h * 100 + my_w
-    else :
-        answer = my_h * (10**(len(str(w)) - 1)) + my_w
-    print(answer)
+    my = [n // h, h] if n % h == 0 else [n // h + 1, n % h]
+    print(my[1] * 100 + my[0])
