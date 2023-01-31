022563616279de37fb5400abf07da74f7f20acd7
MinJee Kim <116625040+nninzy@users.noreply.github.com>
2023-01-31 15:47:57 +0900

    [Bronze II] Title: 대표값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.py
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -1,10 +1,3 @@
-n_dict = {}
-for _ in range(10) :
-    n = int(input())
-    if n in n_dict.keys() : n_dict[n] += 1
-    else : n_dict[n] = 1
-max_cnt = max(n_dict.values()); max_key = 0; sum = 0
-for key, value in n_dict.items() :
-    sum += key * value
-    if value == max_cnt : max_key = key
-print(sum // 10, max_key, sep = '\n')
+n_value = [int(input()) for _ in range(10)]
+n_cnt = [n_value.count(n) for n in n_value]
+print(sum(n_value)//10, n_value[n_cnt.index(max(n_cnt))], sep = '\n')
