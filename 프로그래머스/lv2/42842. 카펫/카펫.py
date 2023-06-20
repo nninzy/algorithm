c2cddd6e2f7c2e870f0276c09f693add2ee18e98
Minjee Kim <116625040+nninzy@users.noreply.github.com>
2023-06-20 18:55:06 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,13 +1,11 @@
 def solution(brown, yellow):
     answer = []
-    y_primes = []
-    for i in range(1, yellow+1):
-        if yellow%i == 0:
-            y_primes.append(i)
-    for num in y_primes:
-        num2 = yellow//num
-        calculate = 2 * (num + num2 + 2)
-        if brown == calculate:
-            answer = [num2 + 2] + [num + 2]
-            break
+    y_primes = []       
+    for num in range(1, yellow+1):
+        if yellow%num == 0:
+            num2 = yellow//num
+            cal = 2 * (num + num2 + 2)
+            if brown == cal:
+                answer = [num2 + 2] + [num + 2]
+                break
     return answer
